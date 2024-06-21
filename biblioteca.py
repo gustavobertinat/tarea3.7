9bac8febc2c5842ec98f912be39cf950eb0bed2f
gustavobertinat <167627953+gustavobertinat@users.noreply.github.com>
2024-06-21 09:36:13 -0300

    Add files via upload

---
+++ biblioteca.py
@@ -0,0 +1,9 @@
+# Lista original de libros
+libros = [102, 511, 205, 110, 310]
+
+# Ordenar los libros en orden decreciente sin modificar la lista original
+libros_ordenados = sorted(libros, reverse=True)
+
+print("Lista original de libros:", libros)
+print("Lista de libros ordenados:", libros_ordenados)
+
---
+++ desafio1.py
@@ -0,0 +1,11 @@
+inventario = ["manzanas", "bananas", "zanahorias", "espinacas", "brocoli", "cebolla",
+"kiwis"]
+
+
+# Pregunta 1: ¿Cuántos tipos de productos hay en el inventario inicial?
+num_productos = len(inventario)
+print(f"Hay {num_productos} tipos de productos en el inventario inicial.")
+
+# Pregunta 2: ¿Qué producto está en la tercera posición del inventario?
+producto_tercera_posicion = inventario[2]
+print(f"El producto en la tercera posición del inventario es {producto_tercera_posicion}.")
